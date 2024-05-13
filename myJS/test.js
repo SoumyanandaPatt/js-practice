45ff2be5ce4f3af4e3a1ee6c4cf1e597c22679d8
SoumyanandaPatt <86007883+SoumyanandaPatt@users.noreply.github.com>
2024-05-13 04:23:23 +0000

    datatypes

---
+++ myJS/test.js
@@ -0,0 +1,15 @@
+// data types
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique
+*/
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
