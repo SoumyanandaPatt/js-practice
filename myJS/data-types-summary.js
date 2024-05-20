251674bba0dc15c7ed69529a30cbc30fed3172db
SoumyanandaPatt <86007883+SoumyanandaPatt@users.noreply.github.com>
2024-05-20 17:22:00 +0000

    Memories in javascript

--- myJS/data-types-summary.js
+++ myJS/data-types-summary.js
@@ -29,3 +29,40 @@ const myFunction = function() {
 }
 console.log(myFunction);
 */
+
+
+
+// ************* Memories **************
+
+/*
+    // Primitive        => Stack memory
+    
+    Example: 1
+    ---------
+    let userDetails = "Ravi Kumar";
+
+    let userCred = userDetails;
+
+    userCred = "RaviK";
+
+    console.log(userDetails);
+    console.log(userCred);
+
+
+    // Non-primitive    => Heap memory
+
+    Example: 2
+    --------
+    let voterDetails = {
+    name: "Somu",
+    age: 25,
+    }
+
+    let vote = voterDetails;
+
+    vote.age = 28;
+    console.log(`my age: ${vote.age}`);
+
+    console.log(`my original age: ${voterDetails.age}`);
+
+*/
