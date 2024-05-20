3582394789962396739647956e6f083b81d5644d
SoumyanandaPatt <86007883+SoumyanandaPatt@users.noreply.github.com>
2024-05-20 17:44:57 +0000

    string concept

---
+++ myJS/string.js
@@ -0,0 +1,14 @@
+/*
+const name = "Somu";
+const repoCount = 5
+
+// console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and repo count is ${repoCount}`);
+
+
+const bookName = new String('Java GOAT');
+console.log(bookName.charAt(0));
+
+
+*/
