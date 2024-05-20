7fa03d8b575518bb676487ed0d132bede4142f27
SoumyanandaPatt <86007883+SoumyanandaPatt@users.noreply.github.com>
2024-05-20 06:11:28 +0000

    conversion of data types

---
+++ myJS/type-conversion.js
@@ -0,0 +1,44 @@
+// const num = 30;
+
+// const num1 = "30abc";
+
+// console.log(typeof num); // number
+
+// console.log(typeof num1); // string
+
+// const num3 = Number(num1); // type conversion to Number type
+// console.log(typeof num3);
+
+// console.log(num3); // If a actual string having number then it will convert to number type. Else it will give result as NaN (Not a Number).
+
+// // Type of NaN in Javascript will be "number" only
+
+// // console.log(typeof null);  => object
+
+// const num4 = null;
+// const nullConvertedNumber = Number(num4);
+
+// console.log(nullConvertedNumber);
+
+
+/*
+    33      => 33
+    "33"    => NaN
+    true    => 1
+*/
+
+// let isLoggedIn = "name";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1    => true; 0  => false;
+// ""   => false
+// "name"=> true
+
+
+let anyNum = 33;
+
+let stringNumber = String(anyNum);
+console.log(typeof stringNumber);
