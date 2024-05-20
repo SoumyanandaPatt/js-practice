f9bbc84b537b2e4d570a0ec9acbedb1c791cfd28
SoumyanandaPatt <86007883+SoumyanandaPatt@users.noreply.github.com>
2024-05-20 16:10:33 +0000

    datatype summary

---
+++ myJS/data-types-summary.js
@@ -0,0 +1,31 @@
+/*
+JS is dynamically typed language
+
+# Primitive
+7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+Examples of Symbol :-
+const a = Symbol('1234');
+const b = Symbol('1234');
+
+console.log(a, b);
+
+console.log(a === b);
+
+
+# Reference (Non-primitive)
+3 types: Object, Array, Functions
+
+
+const heros = ["Shaktiman", "Nagraj", "Doga"];
+
+let myObj = {
+    name: "Somu",
+    age: 27,
+}
+
+const myFunction = function() {
+      console.log("Hello world!");
+}
+console.log(myFunction);
+*/
