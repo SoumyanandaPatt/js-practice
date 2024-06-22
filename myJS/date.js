1e1fa9a1f4b06e9eb786a5de1533d89d08cc4711
SoumyanandaPatt <86007883+SoumyanandaPatt@users.noreply.github.com>
2024-06-22 06:29:29 +0000

    Date and Time concept

---
+++ myJS/date.js
@@ -0,0 +1,19 @@
+//Dates
+
+let myDate = new Date();
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(typeof(myDate));
+
+// let myCreatedDate = new Date(2024, 5, 22);
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp);
+
+/*
+Date in seconds
+*/
+// console.log(Math.floor(myTimeStamp/1000));
